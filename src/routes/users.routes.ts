e341517e25cc8cabb1c6eb84e03184e418004972
Samuel Dantas Cavalcante Evangelista <samuel-icm-dantas@hotmail.com>
2021-01-12 21:18:17 -0300

    Completed MVP

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,50 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import CreateUsersService from '../services/CreateUsersService';
+
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUsersService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ err: err.message });
+    }
+  },
+);
+export default usersRouter;
